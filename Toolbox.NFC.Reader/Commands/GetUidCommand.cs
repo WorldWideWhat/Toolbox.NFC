7ac35bf91aed9e1f153b9cb382d223aaf797725b
Lars Hederidder <coder@worldwidewhat.dk>
2022-09-30 17:55:43 +0200

    Code cleanup

--- Toolbox.NFC.Reader/Commands/GetUidCommand.cs
+++ Toolbox.NFC.Reader/Commands/GetUidCommand.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Toobox.NFC.WinSCard;
-using Toolbox.NFC.Reader.Apdu;
+﻿using Toolbox.NFC.Reader.Apdu;
 using Toolbox.NFC.Reader.Driver;
 
 namespace Toolbox.NFC.Reader.Commands
 {
     public class GetUidCommand : ApduCommand
     {
-        private ReaderType _type;
+        private readonly ReaderType _type;
         public GetUidCommand(ReaderType reader)
         {
             _type = reader;
@@ -28,6 +22,5 @@ namespace Toolbox.NFC.Reader.Commands
                 _ => throw new Exception("Unsupported reader"),
             };
         }
-
     }
 }
