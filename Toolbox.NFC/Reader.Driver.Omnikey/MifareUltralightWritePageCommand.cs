71485243ec112cf4d402e546800ea3c576d31a7a
DarkMixer <coder@worldwidewhat.dk>
2023-03-25 17:26:28 +0100

    Command update for Ultralight read and write functions

--- Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightWritePageCommand.cs
+++ Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightWritePageCommand.cs
@@ -1,18 +1,14 @@
-﻿using Toolbox.NFC.Reader.Apdu;
+﻿using System.Collections.Generic;
+using Toolbox.NFC.Reader.Apdu;
 
 namespace Toolbox.NFC.Reader.Driver.Omnikey
 {
 	internal class MifareUltralightWritePageCommand : ApduCommand
 	{
-		public MifareUltralightWritePageCommand(int pageno, byte[] data)
-			: base(0xFF, 0x0A, 0x00, 0x05)
-		{
-			base.CommandData = new byte[] { 0x01, 0x00, 0xF3, 0x00, 0x00, 0x64, 0xA2, (byte)pageno, 0, 0, 0, 0 };
-			for (var index = 0; index < data.Length; index++)
-			{
-				if (index > 3) break;
-				base.CommandData[index + 8] = data[index];
-			}
-		}
-	}
+        public MifareUltralightWritePageCommand(int pageno, byte[]data)
+            : base(0xFF, 0xD6, 0x00, (byte)pageno, data)
+        {
+            
+        }
+    }
 }
