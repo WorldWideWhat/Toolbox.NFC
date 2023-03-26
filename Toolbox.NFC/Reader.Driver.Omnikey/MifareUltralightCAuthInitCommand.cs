58a9fb734eb229cb4ba580cb7dd44dc7a6c9d741
DarkMixer <coder@worldwidewhat.dk>
2023-03-26 10:15:00 +0200

    Added override function to GetBuffer

--- Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightCAuthInitCommand.cs
+++ Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightCAuthInitCommand.cs
@@ -1,11 +1,28 @@
-﻿using Toolbox.NFC.Reader.Apdu;
+﻿using System.Collections.Generic;
+using Toolbox.NFC.Reader.Apdu;
 
 namespace Toolbox.NFC.Reader.Driver.Omnikey
 {
     internal class MifareUltralightCAuthInitCommand : ApduCommand
     {
         public MifareUltralightCAuthInitCommand()
-            : base(0xFF, 0xA0, 0x00, 0x05,new byte[]{ 0x01, 0x00, 0xF3, 0x00, 0x00, 0x64, 0x1A, 0x00})
-        {}
+            : base(0xFF, 0xA0, 0x00, 0x05, new byte[] { 0x01, 0x00, 0xF3, 0x00, 0x00, 0x64, 0x1A, 0x00 })
+        { }
+
+        public override byte[] GetBuffer()
+        {
+            var buffer = new List<byte>
+            {
+                CLA,
+                INS,
+                P1,
+                P2,
+                (byte)CommandData.Length
+            };
+            buffer.AddRange(CommandData);
+            buffer.Add(0x00);
+
+            return buffer.ToArray();
+        }
     }
 }
--- Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightCAuthorizeCommand.cs
+++ Toolbox.NFC/Reader.Driver.Omnikey/MifareUltralightCAuthorizeCommand.cs
@@ -8,18 +8,27 @@ namespace Toolbox.NFC.Reader.Driver.Omnikey
         public MifareUltralightCAuthorizeCommand(byte[] authKey)
             : base(0xFF, 0xA0, 0x00, 0x05)
         {
-            var payload = new List<byte>()
+            List<byte> payload = new List<byte>()
             {
-                0x01,
-                0x00,
-                0xF3,
-                0x00,
-                0x00,
-                0x64,
-                0xAF
+                0x01, 0x00, 0xF3, 0x00, 0x00, 0x64, 0xAF
             };
             payload.AddRange(authKey);
             base.CommandData = payload.ToArray();
+        }
+
+        public override byte[] GetBuffer()
+        {
+            var buffer = new List<byte>
+            {
+                CLA,
+                INS,
+                P1,
+                P2,
+                (byte)CommandData.Length
+            };
+            buffer.AddRange(CommandData);
+            buffer.Add(0x00);
+            return buffer.ToArray();
         }
     }
 }
