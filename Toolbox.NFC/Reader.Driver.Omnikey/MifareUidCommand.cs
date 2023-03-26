648bb2525724f255bc55490181cc8195e28eb62d
DarkMixer <coder@worldwidewhat.dk>
2023-03-26 10:14:20 +0200

    Renamed class files

---
+++ Toolbox.NFC/Reader.Driver.Omnikey/MifareUidCommand.cs
@@ -0,0 +1,10 @@
+﻿using Toolbox.NFC.Reader.Apdu;
+
+namespace Toolbox.NFC.Reader.Driver.Omnikey
+{
+    internal sealed class MifareUidCommand : ApduCommand
+    {
+        public MifareUidCommand()
+            : base(0xff, 0xCA, 0x00, 0x00) { }
+    }
+}
